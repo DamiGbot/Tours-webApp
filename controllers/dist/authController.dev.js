194bda3be993dcacf5a130b8828ca843f4537559
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-01-30 10:47:31 +0100

    send JWT vis Cookie

--- controllers/dist/authController.dev.js
+++ controllers/dist/authController.dev.js
@@ -26,6 +26,13 @@ var signToken = function signToken(id) {
 
 var createSendToken = function createSendToken(user, statusCode, res) {
   var token = signToken(user._id);
+  var cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true
+  };
+  process.env.NODE_ENV === 'production' ? cookieOptions.secure == true : null;
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token: token,
