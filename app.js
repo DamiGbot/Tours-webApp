d4d89f3e2cab3daaee5131552669d1abc683ce8a
Oluwadamilola Gbotolorun <damigbot@gmail.com>
2022-01-30 21:22:44 +0100

    set security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const { router: tourRouter } = require('./routes/tourRoutes');
 const { router: userRouter } = require('./routes/userRoutes');
@@ -11,10 +12,15 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet({}));
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -22,9 +28,17 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,6 +6,8 @@ var morgan = require('morgan');
 
 var rateLimit = require('express-rate-limit');
 
+var helmet = require('helmet');
+
 var _require = require('./routes/tourRoutes'),
     tourRouter = _require.router;
 
@@ -17,19 +19,28 @@ var AppError = require('./utils/appError');
 var globalErrorHandler = require('./controllers/errorController');
 
 var app = express(); // 1) GLOBAL MIDDLEWARE
+// Set security HTTP headers
+
+app.use(helmet({})); //Development logging
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
-}
+} // limit requests from same API
+
 
 var limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
-app.use('/api', limiter);
-app.use(express.json());
-app.use(express["static"]("".concat(__dirname, "/public")));
+app.use('/api', limiter); // Body parser, reading data from body into req.body
+
+app.use(express.json({
+  limit: '10kb'
+})); // Serving static files
+
+app.use(express["static"]("".concat(__dirname, "/public"))); // Test middleware
+
 app.use(function (req, res, next) {
   req.requestTime = new Date().toISOString();
   next();
